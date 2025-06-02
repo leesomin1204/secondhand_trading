f49c20a491d7082825d8398632910c7de57c8cb7
leesomin1204 <sm1157@naver.com>
2025-06-02 14:10:45 +0900

    소스

---
+++ src/main/java/org/koreait/global/configs/SecurityConfig.java
@@ -0,0 +1,40 @@
+package org.koreait.global.configs;
+
+import org.koreait.member.services.LoginFailureHandler;
+import org.koreait.member.services.LoginSuccessHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        /* 인증 설정 - 로그인, 로그아웃 S */
+        http.formLogin(c -> {
+            c.loginPage("/member/login")
+                    .usernameParameter("email")
+                    .passwordParameter("password")
+                    .successHandler(new LoginSuccessHandler())
+                    .failureHandler(new LoginFailureHandler());
+        });
+
+        http.logout(c -> {
+           c.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                   .logoutSuccessUrl("/member/login");
+        });
+        /* 인증 설정 - 로그인, 로그아웃 E */
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
