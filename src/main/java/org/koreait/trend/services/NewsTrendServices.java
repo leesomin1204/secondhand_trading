d634dbdcda94a4d2d53be564854794a9b651a684
leesomin1204 <sm1157@naver.com>
2025-06-05 11:22:42 +0900

    06.05(1)

--- src/main/java/org/koreait/trend/services/NewsTrendServices.java
+++ src/main/java/org/koreait/trend/services/NewsTrendServices.java
@@ -1,10 +1,33 @@
 package org.koreait.trend.services;
 
-import org.springframework.beans.factory.annotation.Value;
+import lombok.RequiredArgsConstructor;
+import org.koreait.global.configs.PythonProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.WebApplicationContext;
 
+import java.util.Arrays;
+
+@Lazy
 @Service
+@RequiredArgsConstructor
+@EnableConfigurationProperties(PythonProperties.class)
 public class NewsTrendServices {
-    @Value("${python.path.base}")
-    private String pythonDir;
+
+    private final PythonProperties properties;
+    private final WebApplicationContext ctx;
+
+    public void process() {
+        // spring.proriles.active=default,prod // prod가 포함 되어 있으면 production
+        boolean isProduction = Arrays.stream(ctx.getEnvironment().getActiveProfiles())
+                .anyMatch(s -> s.equals("prod"));
+
+        try {
+            ProcessBuilder builder = new ProcessBuilder();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
